f475535da36ba7708f7e85b48da3fdfa8bdd0cd5
chasedreamsd <44153365+chasedreamsd@users.noreply.github.com>
2020-12-15 10:56:35 -0500

    Update sort.cpp

--- algorithm/sort.cpp
+++ algorithm/sort.cpp
@@ -1,5 +1,5 @@
 
-
+// 480 ms
 void rainbowSort(vector<int>& colors, int color_st, int color_ed, int idx_st, int idx_ed){
     if(color_st == color_ed || idx_st == idx_ed){
         return;
@@ -21,4 +21,22 @@ void rainbowSort(vector<int>& colors, int color_st, int color_ed, int idx_st, in
 
     rainbowSort(colors, color_st, color_mid, idx_st, right);
     rainbowSort(colors, color_mid+1, color_ed, left, idx_ed);
+}
+
+// 360 ms
+void rainbowSort2(vector<int>& colors, int color_st, int color_ed, int idx_st, int idx_ed){
+    if(color_st == color_ed || idx_st == idx_ed){
+        return;
+    }
+
+    int color_mid = (color_st + color_ed) / 2;
+    int idx_lo = idx_st;
+    for(int i = idx_st; i <= idx_ed; ++i){
+        if(colors[i] <= color_mid){
+            swap(colors[i], colors[idx_lo++]);
+        }
+    }
+
+    rainbowSort(colors, color_st, color_mid, idx_st, idx_lo-1);
+    rainbowSort(colors, color_mid+1, color_ed, idx_lo, idx_ed);
 }
